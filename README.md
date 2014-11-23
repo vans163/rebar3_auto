3617b86d134bfa598c65f4221a44ad5aa46e3415
Tristan Sloughter <t@crashfast.com>
2014-11-23 15:01:58 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+rebar_auto_plugin
+=====
+
+A rebar3 plugin for auto running tasks on source file change.
+
+Build
+-----
+
+    $ rebar3 compile
+
+Use
+---
+
+Add the plugin to your rebar config:
+
+    {plugins, [
+        {rebar_auto_plugin, ".*", {git, "git@github:tsloughter/rebar_auto_plugin.git", {branch, "master"}}}
+    ]}.
+
+Then just call your plugin directly in an existing application:
+
+
+    $ rebar3 auto
+    ===> Fetching rebar_auto_plugin
+    ===> Compiling rebar_auto_plugin
---
+++ src/rebar_auto_plugin.erl
@@ -0,0 +1,74 @@
+-module(rebar_auto_plugin).
+-behaviour(provider).
+
+-export([init/1, do/1, format_error/1]).
+
+-define(PROVIDER, auto).
+-define(DEPS, []).
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider = providers:create([
+            {name, ?PROVIDER},          % The 'user friendly' name of the task
+            {module, ?MODULE},          % The module implementation of the task
+            {bare, true},               % The task can be run by the user, always true
+            {deps, ?DEPS},              % The list of dependencies
+            {example, "rebar auto"}, % How to use the plugin
+            {opts, []},                  % list of options understood by the plugin
+            {short_desc, "A rebar plugin"},
+            {desc, ""}
+    ]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+    auto(State).
+
+-spec format_error(any()) ->  iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
+
+auto(State) ->
+    rebar_file_monitor:start(),
+    Apps = rebar_state:project_apps(State),
+
+    Refs = lists:map(fun(App) ->
+                             Dir = filename:join(rebar_app_info:dir(App), "src"),
+                             {ok, _, Ref} = rebar_file_monitor:monitor_dir(Dir, self()),
+                             Ref
+                     end, Apps),
+    Task = compile,
+    State1 = rebar_state:set(State, task, Task),
+    State2 = rebar_state:command_args(State1, []),
+    case rebar_core:process_command(State2, Task) of
+        {ok, State3} ->
+            auto(State3, Task, Refs);
+        Error ->
+            Error
+    end.
+
+auto(State, Task, Refs) ->
+    flush(),
+    receive
+        {file_monitor, _, _Msg} ->
+            ok
+    end,
+    {ok, State1} = do(Task, State),
+    auto(State1, Task, Refs).
+
+do(ProviderName, State) ->
+    Provider = providers:get_provider(ProviderName
+                                     ,rebar_state:providers(State)),
+    providers:do(Provider, State).
+
+flush() ->
+    receive
+        _ ->
+            flush()
+    after
+        0 -> ok
+    end.
